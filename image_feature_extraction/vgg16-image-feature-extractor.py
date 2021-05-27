c9aa6b575eae1bc4899fd48514b8812e9e980cbe
KMA Solaiman <ksolaima@purdue.edu>
2021-05-26 20:38:51 -0400

    added different transformations based on data-type - train/test/val

--- image_feature_extraction/vgg16-image-feature-extractor.py
+++ image_feature_extraction/vgg16-image-feature-extractor.py
@@ -9,6 +9,37 @@ if __name__ == '__main__':
     vgg16_model = models.vgg16(pretrained=True)
     vgg16_model.classifier = nn.Sequential(*list(vgg16_model.classifier.children())[:-1])
 
+    '''
+    # https://github.com/pytorch/examples/blob/42e5b996718797e45c46a25c55b031e6768f8440/imagenet/main.py#L89-L101
+    # https://www.kaggle.com/carloalbertobarbano/vgg16-transfer-learning-pytorch
+    # https://pytorch.org/vision/stable/models.html
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    data_transforms = {
+        TRAIN: transforms.Compose([
+            # Data augmentation is a good practice for the train set
+            # Here, we randomly crop the image to 224x224 and
+            # randomly flip it horizontally. 
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+	    normalize
+        ]),
+        VAL: transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+	    normalize
+        ]),
+        TEST: transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+	    normalize
+        ])
+    }
+    '''
+
     transform = T.Compose([
         T.Resize(256),
         T.CenterCrop(224),
