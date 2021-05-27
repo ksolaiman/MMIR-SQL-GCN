dc766e81c1273524768b5869ddd64f3522df8d18
KMA Solaiman <ksolaima@purdue.edu>
2021-05-27 19:39:11 -0400

    old train-test split + Content Edit Distance-with SQL/ folder files

---
+++ SimGNN-on-WLIR+PCAM(train:test-split--Graph-creation--CED-&-GED-calc-by-graph)/report-report-edit-distance.py
@@ -0,0 +1,123 @@
+import psycopg2
+
+# distance problem 
+def editDistDP(str1, str2, m, n): 
+    #print(str1)
+    #print(str2)
+    # Create a table to store results of subproblems 
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)] 
+  
+    # Fill d[][] in bottom up manner 
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+  
+            # If first string is empty, only option is to 
+            # insert all characters of second string 
+            if i == 0: 
+                dp[i][j] = j    # Min. operations = j 
+  
+            # If second string is empty, only option is to 
+            # remove all characters of second string 
+            elif j == 0: 
+                dp[i][j] = i    # Min. operations = i 
+  
+            # If last characters are same, ignore last char 
+            # and recur for remaining string 
+            elif str1[i-1] == str2[j-1]: 
+                dp[i][j] = dp[i-1][j-1] 
+  
+            # If last character are different, consider all 
+            # possibilities and find minimum 
+            else: 
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert 
+                                   dp[i-1][j],        # Remove 
+                                   dp[i-1][j-1])    # Replace 
+  
+    return dp[m][n] 
+  
+
+try:
+    # set-up a postgres connection
+    conn = psycopg2.connect(database='ng', user='ngadmin',password='stonebra',
+                                host='146.148.89.5', port=5432)
+    dbcur = conn.cursor()
+    print("connection successful")
+
+    sql = dbcur.mogrify("""
+    WITH 
+        incident_record AS(
+            select 
+                irr.incident_report_id as iid,
+                irr.incident_report_id_v4 as irrid, 
+                irr.incident_date + irr.incident_time,
+                null, 
+                detected_people.head_color as hc,
+                detected_people.upper_body_color as ubc,
+                detected_people.bottom_body_color as bbc,
+                detected_people.gender as g
+            from 
+                incident_report_record as irr
+            JOIN 
+                incident_report_detected_people as detected_people
+            ON 
+                irr.incident_report_id = detected_people.incident_report_id
+            /* 
+                Following erases the rows which have none of the color codes
+                That kind of report will match with all frames, so no need
+            */
+            AND
+                (
+                    array_length(detected_people.head_color, 1) is not null OR
+                    array_length(detected_people.upper_body_color, 1) is not null OR
+                    array_length(detected_people.bottom_body_color, 1) is not null
+                )
+            AND 
+                irr.incident_report_type NOT LIKE 'Generated_flash%'
+        )
+
+    SELECT
+        A.iid, B.iid, A.g, B.g, lower(A.ubc::text)::text[], lower(B.ubc::text)::text[], 
+        lower(A.hc::text)::text[], lower(B.hc::text)::text[], 
+        lower(A.bbc::text)::text[], lower(B.bbc::text)::text[]
+    FROM incident_record A
+    JOIN incident_record B 
+    ON
+    A.irrid != B.irrid
+    ;
+    """)
+
+    dbcur.execute(sql)
+    data = dbcur.fetchall()
+    leng = 0
+    result = []
+    leng = 0
+    for row in data:
+        #leng+=1
+        result.append(
+            (
+              2, row[0], row[1], 
+              editDistDP(row[4], row[5], len(row[4]), len(row[5])) +
+              editDistDP(row[6], row[7], len(row[6]), len(row[7])) +
+              editDistDP(row[8], row[9], len(row[8]), len(row[9])) + 
+              (1 if row[2] != row[3] else 0)
+             )
+        )
+    print(len(result))
+    
+    for item in result:
+        sql = dbcur.mogrify('INSERT INTO content_edit_distance_realistic (ced_type, uid1, uid2, distance) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING RETURNING ced_id', item)
+        dbcur.execute(sql)
+
+    conn.commit()
+    count = dbcur.rowcount
+    print (count, " Record inserted successfully into mobile table")
+
+except (Exception, psycopg2.Error) as error :
+    if(conn):
+        print("Failed to insert record into mobile table", error)
+
+finally:
+    if(conn):
+        dbcur.close()
+        conn.close()
+
