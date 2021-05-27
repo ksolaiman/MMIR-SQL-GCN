d5c3acefb051faa5c2c6e1f0cab4ee0faa0e4355
KMA Solaiman <ksolaima@purdue.edu>
2021-05-27 19:30:32 -0400

    added what transformations to apply on images

--- image_feature_extraction/vgg16-image-feature-extractor.py
+++ image_feature_extraction/vgg16-image-feature-extractor.py
@@ -40,12 +40,45 @@ if __name__ == '__main__':
     }
     '''
 
+    '''
+    # Original Pedestrian Video Transformations 
+    transform_train = T.Compose([
+            T.Random2DTranslation(args.height, args.width),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    transform_test = T.Compose([
+            T.Resize((args.height, args.width)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    '''
+
     transform = T.Compose([
         T.Resize(256),
         T.CenterCrop(224),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
+
+    # TODO-1: use the data_transforms (from first commented out section) appropriate for the image/video based on it
+    # being training and validation/test data
+    # We are using the data_transforms for vgg16 instead of mars as we are extracting pre-trained vectors
+    # For video, if C3D has any specific transformation (training and test data may have different transformation), follow that
+    
+    # TODO-2: read the mgid for training, validation and testing using the
+    # "for_METU_read_train_val_test_data.py" file;
+    # imagetestset/imagetrainset/ imagevalidset are lists of mgids
+    
+    # TODO-3: read the corresponding row from mmir_ground table for each mgid
+
+    # TODO-4: save the 4096d vector as value with mgid as key for each sample
+
+    # Suggestion: you can pass batch images to vgg-16 instead of 1 at a time
+    # (if you know about it, do it, else it will take more time)
+    
     with open('image_paths.txt', 'r') as image_file:
         for line in image_file:
             image = Image.open(line.rstrip("\n"))
