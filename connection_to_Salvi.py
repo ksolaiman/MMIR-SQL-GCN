cbcd3f4e958eaf7b9172f441da36db7ff551a0c8
KMA Solaiman <ksolaima@purdue.edu>
2025-07-13 15:31:37 -0400

    example db connection

--- connection_to_Salvi.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-# set-up a postgres connection
-conn = psycopg2.connect(database='Salvi', user='Salvi',password='sholock',
-        host='localhost', port=5433)
-dbcur = conn.cursor()
-print("connection successful")
