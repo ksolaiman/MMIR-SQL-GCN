d1b662dab12159acc5289c59c177d76988832391
KMA Solaiman <ksolaima@purdue.edu>
2025-07-14 14:35:20 -0400

    commented out unnecessary parts, added new stt import, and updated func to be run with any data from any other file

--- calc_MAP_mac_1.py
+++ calc_MAP_mac_1.py
@@ -2,51 +2,51 @@ import math
 import pickle
 import scipy
 import numpy as np
-import set_train_test_data_and_distance_matrix_from_mmir_ground_w_localDB as stt
+import set_train_test_data_and_distance_matrix as stt
 # from set_train_test_data_from_mmir_ground import prepare_dataset
 import os
 from matplotlib import pyplot as plt
 from sklearn.metrics import PrecisionRecallDisplay, average_precision_score, precision_recall_curve
 
-with open("predicted_test_distance_matrix.pkl", "rb") as f:
-    dist = pickle.load(f)
-    print(np.shape(dist))
+# with open("predicted_test_distance_matrix.pkl", "rb") as f:
+#     dist = pickle.load(f)
+#     print(np.shape(dist))
     
-#dist = #scipy.spatial.distance.cdist(test, train, metric) # test is query, train is retrieval set, hahahah
-#print(dist[1].reshape(914,2).tolist())
-# ord = dist.argsort(1) # Returns the indices that would sort this array.  ######################### imp
-#print(dist[1].reshape(914,2).tolist())
-# print(ord)
+# #dist = #scipy.spatial.distance.cdist(test, train, metric) # test is query, train is retrieval set, hahahah
+# #print(dist[1].reshape(914,2).tolist())
+# # ord = dist.argsort(1) # Returns the indices that would sort this array.  ######################### imp
+# #print(dist[1].reshape(914,2).tolist())
+# # print(ord)
 
-scores = {} # scores is an empty dict already
+# scores = {} # scores is an empty dict already
 
-if os.path.getsize("testset.pkl") > 0:
-    with open("testset.pkl", "rb") as f:
-        testset = pickle.load(f)
-else:
-    print("size 0")
+# if os.path.getsize("testset.pkl") > 0:
+#     with open("testset.pkl", "rb") as f:
+#         testset = pickle.load(f)
+# else:
+#     print("size 0")
 
 
-if os.path.getsize("trainset.pkl") > 0:
-    with open("trainset.pkl", "rb") as f:
-        trainset = pickle.load(f)
-else:
-    print("size 0")
+# if os.path.getsize("trainset.pkl") > 0:
+#     with open("trainset.pkl", "rb") as f:
+#         trainset = pickle.load(f)
+# else:
+#     print("size 0")
 
-if os.path.getsize("validset.pkl") > 0:
-    with open("validset.pkl", "rb") as f:
-        validset = pickle.load(f)
-else:
-    print("size 0")
+# if os.path.getsize("validset.pkl") > 0:
+#     with open("validset.pkl", "rb") as f:
+#         validset = pickle.load(f)
+# else:
+#     print("size 0")
 
-with open("testsetitem2idx.pkl", "rb") as f:
-    item2idx = pickle.load(f)
-with open("testsetidx2item.pkl", "rb") as f:
-    idx2item = pickle.load(f)
+# with open("testsetitem2idx.pkl", "rb") as f:
+#     item2idx = pickle.load(f)
+# with open("testsetidx2item.pkl", "rb") as f:
+#     idx2item = pickle.load(f)
 
-with open("test_distance_matrix.pkl", "rb") as f:
-    ground_dist = pickle.load(f)
-    print(np.shape(ground_dist))
+# with open("test_distance_matrix.pkl", "rb") as f:
+#     ground_dist = pickle.load(f)
+#     print(np.shape(ground_dist))
 
 def calPrecision(y1, x1, y2, x2, x):
     if x2 == x1:
@@ -54,6 +54,12 @@ def calPrecision(y1, x1, y2, x2, x):
     result = (y2-y1)*(x-x1)*1.0/(x2-x1) + y1;
     return result
 
+# NOTE: This function was originally added as an experimental alternative using scikit-learn's average_precision_score.
+# It is NOT suitable for retrieval ranking MAP evaluation in this project.
+# average_precision_score treats distances as uncalibrated binary classification scores and thresholds them globally,
+# ignoring per-query ranking. This loses the core ranking-based nature of retrieval MAP, where results must be
+# sorted for each query and precision measured at each rank. For correct retrieval evaluation, use the manual
+# ranking-based fx_calc_map_label_detailed() instead.
 def sklearn_calc_map_and_PR(db_items, query_items, dist, ground_dist, k):
     numcases = len(db_items)
     query_indices = [item2idx[item] for item in query_items]
@@ -77,7 +83,7 @@ def sklearn_calc_map_and_PR(db_items, query_items, dist, ground_dist, k):
 
     return (np.mean(AP))
 
-def fx_calc_map_label_detailed(db_items, query_items, dist, ground_dist, k):
+def fx_calc_map_label_detailed(db_items, query_items, dist, ground_dist, item2idx, k):
     numcases = len(db_items)  # train_labels.shape[0]
     query_indices = [item2idx[item] for item in query_items]
     db_indices = [item2idx[item] for item in db_items]
@@ -152,7 +158,9 @@ def fx_calc_map_label_detailed(db_items, query_items, dist, ground_dist, k):
             for recallLevel in np.arange(0, 1.1, 0.1):  # 0.001 with 0.001 step until 1: 1000 iterations
                 # For instance for recall level of 0.2, we need to find the maximum precision where recall is greater than
                 # or equal to 0.2 in original recall precision table.
-                indices_w_greater_recall = [idx for idx, v in enumerate(recall[query_no]) if v > recallLevel]
+                indices_w_greater_recall = [idx for idx, v in enumerate(recall[query_no]) if v > recallLevel]  
+                # Official IR formulation: For each standardized recall level r, find the maximum precision achieved at any recall ≥ r. 
+                # So in new calc_MAP function i changed to v >= recallLevel
                 precisions = [precision[query_no][idx] for idx in indices_w_greater_recall]
                 if len(precisions) != 0:
                     precisionValue[query_no][count] = max(precisions)
@@ -177,93 +185,93 @@ def fx_calc_map_label_detailed(db_items, query_items, dist, ground_dist, k):
     print(res)
     return res
 
-# dist will change based on what model we test  - SimGNN, SDML or SQL.
-# ground_dist will always remain same
-# subset of dist is selected in the fx_calc_map_label() function, that is to select subset of items for image/text/video subset test
-#############################
-# np.random.seed(1234)
-# randtest = np.random.choice(testset, 10)
-# randtrain = np.random.choice(testset, 3)
-#############################
-#print(fx_calc_map_label(testset, testset, dist, ground_dist, k = -1))
-#print(fx_calc_map_label(randtrain, randtest, dist, ground_dist, k = 0)) #-1 sets _k to 50
-# input()
-rows = stt.prepare_dataset()
-print("############# Start of size of the dataset #################")
-texttestset = [row[5] for row in rows if row[5]]
-imagetestset = [row[3] for row in rows if row[3]]
-videotestset = [row[4] for row in rows if row[4]]
-print("test-text:", len(texttestset))
-print(len(imagetestset))
-print(len(videotestset))
+# # dist will change based on what model we test  - SimGNN, SDML or SQL.
+# # ground_dist will always remain same
+# # subset of dist is selected in the fx_calc_map_label() function, that is to select subset of items for image/text/video subset test
+# #############################
+# # np.random.seed(1234)
+# # randtest = np.random.choice(testset, 10)
+# # randtrain = np.random.choice(testset, 3)
+# #############################
+# #print(fx_calc_map_label(testset, testset, dist, ground_dist, k = -1))
+# #print(fx_calc_map_label(randtrain, randtest, dist, ground_dist, k = 0)) #-1 sets _k to 50
+# # input()
+# rows = stt.prepare_dataset()
+# print("############# Start of size of the dataset #################")
+# texttestset = [row[5] for row in rows if row[5]]
+# imagetestset = [row[3] for row in rows if row[3]]
+# videotestset = [row[4] for row in rows if row[4]]
+# print("test-text:", len(texttestset))
+# print(len(imagetestset))
+# print(len(videotestset))
 
-texttrainset = [row[8] for row in rows if row[8]]
-imagetrainset = [row[6] for row in rows if row[6]]
-videotrainset = [row[7] for row in rows if row[7]]
-print("train-text:", len(texttrainset))
-print(len(imagetrainset))
-print(len(videotrainset))
-textvalidset = [row[11] for row in rows if row[11]]
-imagevalidset = [row[9] for row in rows if row[9]]
-videovalidset = [row[10] for row in rows if row[10]]
-print("valid-text:", len(textvalidset))
-print(len(imagevalidset))
-print(len(videovalidset))
+# texttrainset = [row[8] for row in rows if row[8]]
+# imagetrainset = [row[6] for row in rows if row[6]]
+# videotrainset = [row[7] for row in rows if row[7]]
+# print("train-text:", len(texttrainset))
+# print(len(imagetrainset))
+# print(len(videotrainset))
+# textvalidset = [row[11] for row in rows if row[11]]
+# imagevalidset = [row[9] for row in rows if row[9]]
+# videovalidset = [row[10] for row in rows if row[10]]
+# print("valid-text:", len(textvalidset))
+# print(len(imagevalidset))
+# print(len(videovalidset))
 
-####################
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/imagetestset.pkl", "wb") as f:
-#     pickle.dump(imagetestset, f)
-# with open("latest-simgnn/data/testset/videotestset.pkl", "wb") as f:
-#     pickle.dump(videotestset, f)
-#############
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
-# with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
-#     pickle.dump(texttestset, f)
+# ####################
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/imagetestset.pkl", "wb") as f:
+# #     pickle.dump(imagetestset, f)
+# # with open("latest-simgnn/data/testset/videotestset.pkl", "wb") as f:
+# #     pickle.dump(videotestset, f)
+# #############
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
+# # with open("latest-simgnn/data/testset/texttestset.pkl", "wb") as f:
+# #     pickle.dump(texttestset, f)
 
-print("############# Start of calc. MAP #################")
-# print(sklearn_calc_map_and_PR(texttestset, texttestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(videotestset, texttestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(imagetestset, texttestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(testset, texttestset, dist, ground_dist, k = 0))
-# print("########")
-# #
-# print(sklearn_calc_map_and_PR(videotestset, videotestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(texttestset, videotestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(imagetestset, videotestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(testset, videotestset, dist, ground_dist, k = 0))
-# print("########")
-# #
-# print(sklearn_calc_map_and_PR(imagetestset, imagetestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(texttestset, imagetestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(videotestset, imagetestset, dist, ground_dist, k = 0))
-# print(sklearn_calc_map_and_PR(testset, imagetestset, dist, ground_dist, k = 0))
-# print("########")
+# print("############# Start of calc. MAP #################")
+# # print(sklearn_calc_map_and_PR(texttestset, texttestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(videotestset, texttestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(imagetestset, texttestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(testset, texttestset, dist, ground_dist, k = 0))
+# # print("########")
 # # #
-maps = []
-maps.append(fx_calc_map_label_detailed(texttestset, texttestset, dist, ground_dist, k = 0))         # !!
-maps.append(fx_calc_map_label_detailed(videotestset, texttestset, dist, ground_dist, k = 0))
-maps.append(fx_calc_map_label_detailed(imagetestset, texttestset, dist, ground_dist, k = 0))
-maps.append(fx_calc_map_label_detailed(testset, texttestset, dist, ground_dist, k = 0))
-#
-maps.append(fx_calc_map_label_detailed(videotestset, videotestset, dist, ground_dist, k = 0))
-maps.append(fx_calc_map_label_detailed(texttestset, videotestset, dist, ground_dist, k = 0)) #
-maps.append(fx_calc_map_label_detailed(imagetestset, videotestset, dist, ground_dist, k = 0))
-maps.append(fx_calc_map_label_detailed(testset, videotestset, dist, ground_dist, k = 0))
-#
-maps.append(fx_calc_map_label_detailed(imagetestset, imagetestset, dist, ground_dist, k = 0))
-maps.append(fx_calc_map_label_detailed(texttestset, imagetestset, dist, ground_dist, k = 0))  #
-maps.append(fx_calc_map_label_detailed(videotestset, imagetestset, dist, ground_dist, k = 0))   #
-maps.append(fx_calc_map_label_detailed(testset, imagetestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(videotestset, videotestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(texttestset, videotestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(imagetestset, videotestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(testset, videotestset, dist, ground_dist, k = 0))
+# # print("########")
+# # #
+# # print(sklearn_calc_map_and_PR(imagetestset, imagetestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(texttestset, imagetestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(videotestset, imagetestset, dist, ground_dist, k = 0))
+# # print(sklearn_calc_map_and_PR(testset, imagetestset, dist, ground_dist, k = 0))
+# # print("########")
+# # # #
+# maps = []
+# maps.append(fx_calc_map_label_detailed(texttestset, texttestset, dist, ground_dist, k = 0))         # !!
+# maps.append(fx_calc_map_label_detailed(videotestset, texttestset, dist, ground_dist, k = 0))
+# maps.append(fx_calc_map_label_detailed(imagetestset, texttestset, dist, ground_dist, k = 0))
+# maps.append(fx_calc_map_label_detailed(testset, texttestset, dist, ground_dist, k = 0))
+# #
+# maps.append(fx_calc_map_label_detailed(videotestset, videotestset, dist, ground_dist, k = 0))
+# maps.append(fx_calc_map_label_detailed(texttestset, videotestset, dist, ground_dist, k = 0)) #
+# maps.append(fx_calc_map_label_detailed(imagetestset, videotestset, dist, ground_dist, k = 0))
+# maps.append(fx_calc_map_label_detailed(testset, videotestset, dist, ground_dist, k = 0))
+# #
+# maps.append(fx_calc_map_label_detailed(imagetestset, imagetestset, dist, ground_dist, k = 0))
+# maps.append(fx_calc_map_label_detailed(texttestset, imagetestset, dist, ground_dist, k = 0))  #
+# maps.append(fx_calc_map_label_detailed(videotestset, imagetestset, dist, ground_dist, k = 0))   #
+# maps.append(fx_calc_map_label_detailed(testset, imagetestset, dist, ground_dist, k = 0))
 
-print(np.mean(maps))
+# print(np.mean(maps))
